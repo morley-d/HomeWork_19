1e503b0ef996d955b94a04b406e5e6c1a7a6eec2
Andrey <21elektro87@gmail.com>
2022-07-26 06:00:25 +0600

    username is unique

--- app/dao/model/user.py
+++ app/dao/model/user.py
@@ -6,7 +6,7 @@ from app.setup_db import db
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     role = db.Column(db.String(255))
 
